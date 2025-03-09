20c507d0e260935d0c3df93b769bac532cf1b737
luisgon-dev <gonzalezdavidr22@gmail.com>
2025-03-08 22:43:16 -0800

    Change Summoner Gathering to Daily (#2)

--- ProjectSyndraBackend.Service/Services/RiotApi/Implementations/MatchDataGatheringService.cs
+++ ProjectSyndraBackend.Service/Services/RiotApi/Implementations/MatchDataGatheringService.cs
@@ -12,7 +12,7 @@ public class MatchDataGatheringService(IBackgroundJobClient backgroundJobClient)
         // every hour check to see if a new patch is available for league.
         RecurringJob.AddOrUpdate<UpdateParameters>("addorupdate", x => x.Execute(CancellationToken.None), Cron.Hourly);
         RecurringJob.AddOrUpdate<AddOrUpdateHighEloProfiles>("fetchHighEloPlayers",
-            x => x.Execute(CancellationToken.None), Cron.Hourly);
+            x => x.Execute(CancellationToken.None), Cron.Daily);
         RecurringJob.AddOrUpdate<FetchLatestMatchInformation>("fetchLatestMatchInformation",
             x => x.Execute(CancellationToken.None), Cron.Hourly);
     }
